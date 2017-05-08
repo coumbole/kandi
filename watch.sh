48906d99388131eacca979991fb2763aae0720d4
coumbole <ville.kumpulainen@gmail.com>
2017-05-08 14:59:01 +0300

    Initialized template for the thesis and for the research plan

---
+++ watch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+inotifywait -mq -e close_write,moved_to --format %e/%f . |
+while IFS=/ read -r events file; do
+  if [ "$file" = "thesis.tex" ]; then
+      echo $file
+      pdflatex $file > pdflatex.log
+  fi
+done
