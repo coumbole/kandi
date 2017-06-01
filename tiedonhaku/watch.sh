1dcbd949ed71425f15195bf9407294b7300850d3
Ville Kumpulainen <ville.kumpulainen@relexsolutions.com>
2017-06-01 22:22:56 +0300

    tiedonhaun harjoitus tehty

---
+++ tiedonhaku/watch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+inotifywait -mq -e close_write,moved_to --format %e/%f . |
+while IFS=/ read -r events file; do
+  if [ "$file" = "tiedonhaku.tex" ]; then
+      echo $file
+      pdflatex $file > pdflatex.log
+  fi
+done
